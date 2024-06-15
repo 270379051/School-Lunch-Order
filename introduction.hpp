db14a935508528351cccbf4fa82974848e69eec6
1322harsh <270449050@yoobeestudent.ac.nz>
2024-06-15 21:16:53 +1200

    Add files via upload

---
+++ introduction.hpp
@@ -0,0 +1,14 @@
+//
+//  introduction.hpp
+//  introduction
+//
+//  Created by Harshdeep Singh on 15/06/2024.
+//
+
+#ifndef introduction_hpp
+#define introduction_hpp
+
+#include <iostream>
+using namespace std;
+void printHeading();
+#endif /* introduction_hpp */
---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  introduction
+//
+//  Created by Harshdeep Singh on 15/06/2024.
+//
+
+#include <iostream>
+int main()
+{
+    printHeading();
+}
+
+
