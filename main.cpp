1acc55da88258eec13d34d31abf00cd2c0a11721
1322harsh <270449050@yoobeestudent.ac.nz>
2024-06-16 18:37:54 +1200

    function call : registration parent

--- main.cpp
+++ main.cpp
@@ -9,10 +9,31 @@
 int main()
 {
    
-   
+    while (true) {
         printHeading();
         list_options();
-        get_options();
+
+        int selected_option = get_option(); // Get the user's task choice
+        switch (selected_option) {
+            
+         
+            case 3:
+                printHeading();
+                login_registerparent();
+                break;
+
+            default:
+                cout << "Invalid option!" << endl;
+                break;
+        }
+
+        cout << "Press Enter to return to the main menu...";
+        cin.ignore();
+        cin.get();
+    }
+        printHeading();
+        list_options();
+        get_option();
         login_registerparent();
 }
 
