b82d740f26fb3a335d547a8e691dcb550b392074
1322harsh <270449050@yoobeestudent.ac.nz>
2024-06-29 11:28:19 +1200

    function declarations

--- adminscreen.hpp
+++ adminscreen.hpp
@@ -1,7 +1,18 @@
+#ifndef ADMINCREEN_HPP
+#define ADMINSCREEN_HPP
+
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 const string ADMIN_USERNAME = "admin@thelunchspot";//admin username and password
 const string ADMIN_PASSWORD = "admin123";
+vector<string> reviewlist;// vector to store responded complaints
 void admin_login();
+void adminreview() ;
+void adminreviewstaff();
+void changeMenu();
+void listOrdersFromSameDay();
+void pendingPayment();
+
+#endif
