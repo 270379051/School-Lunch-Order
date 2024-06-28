ec903d59c4db155392f9eb6430e86b84d9f6f175
1322harsh <270449050@yoobeestudent.ac.nz>
2024-06-29 11:25:30 +1200

    Create adminscreen.hpp

---
+++ adminscreen.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+const string ADMIN_USERNAME = "admin@thelunchspot";//admin username and password
+const string ADMIN_PASSWORD = "admin123";
+void admin_login();
