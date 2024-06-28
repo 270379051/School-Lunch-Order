d75c3799ca96c1aaa81d1becfc46663f45b068f1
1322harsh <270449050@yoobeestudent.ac.nz>
2024-06-29 11:34:19 +1200

    code refactoring

--- main.cpp
+++ main.cpp
@@ -6,50 +6,47 @@
 //
 
 #include "introduction.hpp"
-int main()
-{
-   
+#include "adminscreen.hpp"
+int main() {
     while (true) {
         printHeading();
         list_options();
-
+        
         int selected_option = get_option(); // Get the user's task choice
         switch (selected_option) {
-            
-           case 1:
-               printheading();
-               weekly_menu();
-              break;
-           case 2:
-               printheading();
-               display_discounts();
-              break;
+            case 1:
+                printHeading();
+                wholemenu();
+                break;
+            case 2:
+                printHeading();
+                todays_menu();
+                break;
             case 3:
+                printHeading();
+                display_discounts();
+                break;
+            case 4:
                 printHeading();
                 login_registerparent();
                 break;
-           case 4:
-                printHeading();
-               login_registerstaff();
-                break;
             case 5:
+                printHeading();
+                login_registerstaff();
+                break;
+            case 6:
                 printHeading();
                 admin_login();
                 break;
-           case 6:
-                printHeading();
-                contact_details();
-                break;
-
             default:
                 cout << "Invalid option!" << endl;
                 break;
         }
-
+        
         cout << "Press Enter to return to the main menu...";
         cin.ignore();
         cin.get();
     }
-       
 
-
+    return 0;
+}
