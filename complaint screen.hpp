0130fa8b2e67071bfaa066567406a056ac5ea1cd
270379051 <270379051@yoobeestudent.ac.nz>
2024-07-01 23:18:38 +1200

    final refactoring

--- complaint screen.hpp
+++ complaint screen.hpp
@@ -1,21 +1,13 @@
-#ifndef COMPLAINTSCREEN_HPP
-#define COMPLAINTSCREEN_HPP
-#include <iostream>
-#include <string>
+
+#ifndef complaintscreen_hpp
+#define complaintscreen_hpp
+#include <fstream>
 #include <vector>
 using namespace std;
-struct Recent{// structure for reecentparent to be used in complaint and order functions
-    string name;
-    string contact;
-};
-vector<string> complaintlist;// vector to store complaint names
-vector<Recent> recentparent;//vector to store recentparent in complaint and order function
-vector<string> staffComplaintList;//vector to store staff compalaint list
-vector<Recent> recentstaff;//vector to store recentstaff in complaint and order function
+extern vector<string> complaintlist;
+extern vector<string> staffComplaintList;
 void lodgecomplaint();
-void lodgestaffcomplaint();
 void parentreview();
 void staffreview();
-void parentcomplaint();
-void staffcomplaint();
-#endif
+void lodgestaffcomplaint();
+#endif /* complaintscreen_hpp */
